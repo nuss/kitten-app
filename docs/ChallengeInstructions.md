cbadaf1ea9202c1fc3d80a9c8ff4356dadc27a63
shauzmayer <sandra.hauzmayer@uni-ak.ac.at>
2019-07-22 11:49:47 +0200

    init

---
+++ docs/ChallengeInstructions.md
@@ -0,0 +1,19 @@
+# coding-callenge
+
+####Instructions:
+
+This folder contains a basic set up for a [Vue](https://vuejs.org/v2/) project (set up with Vue-Cli 3) called "My Kitten App".
+The task would be to create a page that that looks similar to the one in the [mock up included](Mock_Up.png)
+and has the following functionality:
+
+- on top a "kitten of the day" in a card like element, with like / dislike icon (does not need to be functional)
+- below kittens to choose from with a heart icon (does not need to be functional, however only kittens with property "isFavourite" true
+  should have a filled heart).
+  
+  (all necessary images, icons and data can be found in [../src/assets]())
+- upon clicking on one of the choosable kittens it will be placed on top as "kitten of the day" and the previous kitten of the day should go
+  back into the list of choosable kittens.
+- if you see any other issues with the code feel free to improve it.
+
+
+Please return your solution to us until the end of 28.07.2019.
---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <div>My Kitten App</div>
+    <div>
+      <div>Kitten of the day:</div>
+      <div>
+        <div v-if="selectedKitten">{{ selectedKitten.name }}</div>
+        <img
+            :src="require('./assets/images/' + selectedKitten.img)"
+            class="kitten-image">
+      </div>
+    </div>
+    <div>
+      <div>Select your favourite kitten:</div>
+      <div v-for="kitten in filteredKittens">
+        <div>
+          {{ kitten.name }}
+        </div>
+        <img
+          :src="require('./assets/images/' + kitten.img)"
+          class="kitten-image">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { KITTEN_DATA } from './assets/data';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      kittens: KITTEN_DATA,
+      selectedKitten: null,
+    };
+  },
+  computed: {
+    // return all kittens except the currently selected
+    filteredKittens() {
+      return this.kittens
+        .filter(kitten => kitten.name !== this.selectedKitten.name);
+    }
+  },
+  created() {
+    // set the first kitten as "kitten of the day"
+    this.selectedKitten = this.kittens[0];
+  }
+}
+</script>
+
+<style>
+html {
+  background-color: #F0F0F0;
+}
+
+h1 {
+  font-size: 36px;
+  font-weight: bolder;
+  font-family: 'Raleway', Helvetica, Arial, sans-serif;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+h5 {
+  font-weight: bolder;
+  margin: 0;
+}
+
+#app {
+  font-family: 'Lato', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+  text-align: center;
+  color: #2c3e50;
+  margin-top:30px;
+}
+
+.kitten-image {
+  max-height: 100px;
+}
+
+.kitten-card {
+  background: white;
+  border-radius: 4px;
+  box-shadow: 0 0 3px 0 rgba(0,0,0,.05);
+  text-align: left;
+}
+</style>
